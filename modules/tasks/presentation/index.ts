40eaccc7ab72ea905639d5db4f4041121911dee9
Felix Lee <leeyoongti@gmail.com>
2026-07-19 17:04:04 +0800

    refactor: remove obsolete inbox surface

--- modules/tasks/presentation/index.ts
+++ modules/tasks/presentation/index.ts
@@ -1,8 +1,6 @@
-export { InboxScreen } from "./InboxScreen";
 export { TaskDetailScreen } from "./TaskDetailScreen";
 export { TaskCommandPalette } from "./TaskCommandPalette";
 export { TaskNavigation } from "./TaskNavigation";
 export type { TaskNavigationCurrent } from "./TaskNavigation";
 export { TaskWorkspaceScreen } from "./TaskWorkspaceScreen";
 export type { TaskWorkspaceDestination } from "./TaskWorkspaceScreen";
-export type { InboxScreenProps } from "./InboxScreen";
